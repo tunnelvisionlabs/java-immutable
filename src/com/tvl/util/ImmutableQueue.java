84687d7aa8b05e5f7dbac628e31b46ef632d37e0
Sam Harwell <sam@tunnelvisionlabs.com>
2015-05-20 18:36:20 -0500

    Add the ImmutableQueue interface

---
+++ src/com/tvl/util/ImmutableQueue.java
@@ -0,0 +1,49 @@
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+package com.tvl.util;
+
+import java.util.EmptyStackException;
+
+/**
+ * An immutable queue.
+ *
+ * @param <T> The type of elements in the queue.
+ */
+public interface ImmutableQueue<T> extends Iterable<T> {
+
+    /**
+     * Gets a value indicating whether this is the empty queue.
+     *
+     * @return {@code true} if this queue is empty; otherwise {@code false}.
+     */
+    boolean isEmpty();
+
+    /**
+     * Gets an empty queue.
+     */
+    ImmutableQueue<T> clear();
+
+    /**
+     * Gets the element at the front of the queue.
+     *
+     * @return The element at the front of the queue.
+     * @throws EmptyStackException if the queue is empty.
+     */
+    T peek();
+
+    /**
+     * Adds an element to the back of the queue.
+     *
+     * @param value The element to add to the queue.
+     * @return The new queue.
+     */
+    ImmutableQueue<T> enqueue(T value);
+
+    /**
+     * Returns a queue that is missing the front element.
+     *
+     * @return The new queue; never {@code null}.
+     * @throws EmptyStackException if the queue is empty.
+     */
+    ImmutableQueue<T> dequeue();
+
+}
